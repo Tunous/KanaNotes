c7685e1d41822e0d5676dca1783db0cada8e6f2f
Tunous <lukus178@gmail.com>
2016-12-25 17:04:11 +0100

    Add list creation code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,38 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::addList()
+{
+    QLineEdit *listNameInput = new QLineEdit();
+    QToolButton *listActionsButton = new QToolButton();
+    listActionsButton->setText("...");
+
+    QHBoxLayout *titleRowLayout = new QHBoxLayout();
+    titleRowLayout->addWidget(listNameInput);
+    titleRowLayout->addWidget(listActionsButton);
+
+    QWidget *titleRow = new QWidget();
+    titleRow->setLayout(titleRowLayout);
+
+    QTextEdit *note = new QTextEdit();
+
+    QPushButton *addNoteButton = new QPushButton();
+    addNoteButton->setText("Add a note...");
+
+    QVBoxLayout *listLayout = new QVBoxLayout();
+    listLayout->addWidget(titleRow);
+    listLayout->addWidget(note);
+    listLayout->addWidget(addNoteButton);
+
+    QWidget *list = new QWidget();
+    list->setMinimumWidth(250);
+    list->setMaximumWidth(250);
+    list->setLayout(listLayout);
+
+    QLayout *listContainerLayout = ui->listContainer->layout();
+    QLayoutItem *lastItem = listContainerLayout->takeAt(listContainerLayout->count() - 1);
+
+    listContainerLayout->addWidget(list);
+    listContainerLayout->addItem(lastItem);
+}
